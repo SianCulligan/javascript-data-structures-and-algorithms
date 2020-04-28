d60d3321e47d0c20aa68c1f87ffa98e830822604
Sian Culligan <SianCulligan@gmail.com>
2020-04-28 00:33:17 -0700

    tests passing, good to go!

--- code_challenges/401/fifoAnimalShelter/fifo-animal-shelter.js
+++ code_challenges/401/fifoAnimalShelter/fifo-animal-shelter.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// const Animal = require('./animal.js');
-
 class Node {
   constructor(val) {
     this.val = val;
@@ -17,97 +15,53 @@ class AnimalShelter {
 
   enqueue(animal) {
     let newNode = new Node(animal);
-    if(!this.front && !this.rear){
+    if (!this.front && !this.rear) {
       this.front = newNode;
       this.rear = newNode;
       return;
     }
-
-    // if (this.front === this.rear) {
-    //   this.rear = newNode;
-    //   this.front.necxt = newNode;
-    // }
-
-    // let tempRear = this.rear;
-
     this.rear.next = newNode;
-    this.rear = newNode
-
-    // if(this.isempty()) {
-    //   this.front = newAnimal;
-    //   this.rear = newAnimal;
-    //   return;
-    // }
-    // this.rear.next = newAnimal;
-    // this.rear = newAnimal;
-    // return newAnimal;
+    this.rear = newNode;
   }
 
-  dequeue (pref) {
-    //traversal is always O(n)
-    //start @ front, traverse until
-    if (!pref === 'dog' || !pref ==='cat' ) return null;
-
+  dequeue(pref) {
+    if (!(pref === 'dog' || pref === 'cat')) return null;
     let currentNode = this.front;
     let prevNode = null;
-
-
     while (currentNode) {
       let animal = currentNode.val;
-
       if (animal.species === pref) {
         if (!prevNode) {
-          let dequeuedNode = currentNode;
           this.front = currentNode.next;
-          dequeuedNode.next = null;
+          currentNode.next = null;
           return currentNode;
         }
         prevNode.next = currentNode.next;
-
-        if(!currentNode.next)
-          this.rear = prevNode;
-
+        if (!currentNode.next) this.rear = prevNode;
         currentNode.next = null;
         return currentNode;
       }
       prevNode = currentNode;
       currentNode = currentNode.next;
     }
-
     return null;
   }
 
-//   isempty () {
-//     if(!this.front && !this.rear) {
-//       return true;
-//     }else{
-//       return false;
-//     }
-//   }
+  toString() {
+    let currentNode = this.front;
+    let str = '';
+    while (currentNode) {
+      str +=
+                '[ name: ' +
+                currentNode.val.name +
+                ', species: ' +
+                currentNode.val.species +
+                '] -> ';
+      currentNode = currentNode.next;
+    }
+    str += 'null';
+    return str;
+  }
 }
-
-
-// const myList = new AnimalShelter;
-
-// myList.enqueue('1');
-// console.log('LIST', myList);
-// myList.enqueue('2');
-// console.log('LIST', myList);
-// myList.enqueue('3');
-// console.log('LIST', myList);
-// myList.enqueue('4');
-// console.log('LIST', myList);
-// myList.dequeue();
-// console.log('LIST', myList);
-
-// let fido = new Dog('fido');
-// AnimalShelter.enqueue(fido)
-// console.log(fido);
-// console.log('SHELTER', AnimalShelter);
-
-// let felix = new Cat('felix');
-// AnimalShelter.enqueue(felix)
-// console.log(felix);
-// console.log('SHELTER', AnimalShelter);
 
 module.exports = AnimalShelter;
--- code_challenges/401/fifoAnimalShelter/fifo.test.js
+++ code_challenges/401/fifoAnimalShelter/fifo.test.js
@@ -1,35 +1,59 @@
 'use strict';
 
+
 const AnimalShelter = require('./fifo-animal-shelter.js');
-const Animal = require('./animal.js');
+const Animal = require('./animals.js');
 
 
-describe ('happy path', () => {
-  it('Can successfully enqueue into a queue', () => {
-    let fido = new Animal('fido');
-    let list = new AnimalShelter;
 
-    list.enqueue(fido);
-    console.log('LIST',list)
-    console.log('FIDO',fido)
-    expect(list).toBeTruthy;
+describe('happy path', () => {
+  it('Can successfully enqueue', () => {
+    let littleton = new Animal.Dog('littleton');
+    let newShelter = new AnimalShelter;
+    newShelter.enqueue(littleton);
+    let str = newShelter.front.val.name;
+    expect(str).toBe('littleton');
   });
 
-  // describe ('edge cases', () => {
-  //     it('Calling dequeue or peek on empty queue raises exception', () => {
-  //         let myQueue = new Queue();
+  it('Can enqueue multiple pets', () => {
+    let littleton = new Animal.Dog('littleton');
+    let marvel = new Animal.Dog('marvel');
+    let stormageddon = new Animal.Cat('stormageddon');
+    let newShelter = new AnimalShelter;
+    newShelter.enqueue(littleton);
+    newShelter.enqueue(stormageddon);
+    newShelter.enqueue(marvel);
+    let str = newShelter.front.val.name;
+    let strTwo = newShelter.rear.val.name;
+    let strThree = newShelter.front.next.val.name;
+    expect(str).toBe('littleton');
+    expect(strTwo).toBe('marvel');
+    expect(strThree).toBe('stormageddon');
+  });
 
-  //         expect(myQueue.isempty()).toBeTruthy;
-  //         expect(myQueue.front).toBe(null);
+  it('Can successfully dequeue', () => {
+    let littleton = new Animal.Dog('littleton');
+    let marvel = new Animal.Dog('marvel');
+    let stormageddon = new Animal.Dog('stormageddon');
+    let woodrow = new Animal.Cat ('woodrow');
+    let newShelter = new AnimalShelter;
 
-  //         expect(() => {
-  //           myQueue.peek();
-  //         }).toThrow();
-  //         expect(() => {
-  //           myQueue.dequeue();
-  //         }).toThrow();
-  // });
+    newShelter.enqueue(littleton);
+    newShelter.enqueue(stormageddon);
+    newShelter.enqueue(marvel);
+    newShelter.enqueue(woodrow);
+    expect(newShelter.toString()).toBe('[ name: littleton, species: dog] -> [ name: stormageddon, species: dog] -> [ name: marvel, species: dog] -> [ name: woodrow, species: cat] -> null');
 
-  // describe ('expected failures', () => {
+    newShelter.dequeue('cat');
 
+    expect(newShelter.toString()).toBe('[ name: littleton, species: dog] -> [ name: stormageddon, species: dog] -> [ name: marvel, species: dog] -> null');
+  });
+});
+
+describe('edge cases', () => {
+  it('Does not break if shelter is empty', () => {
+    let newShelter = new AnimalShelter;
+    console.log('WHATS THIS', newShelter.front);
+    expect(newShelter.front).toBe(null);
+  });
 });
