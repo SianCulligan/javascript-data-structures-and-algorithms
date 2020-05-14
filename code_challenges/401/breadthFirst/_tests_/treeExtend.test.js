d8e97441292f9c2d8db370bc53cb165c99eb265b
Sian Culligan <SianCulligan@gmail.com>
2020-05-13 18:53:00 -0700

    refactoring tests

--- code_challenges/401/breadthFirst/_tests_/treeExtend.test.js
+++ code_challenges/401/breadthFirst/_tests_/treeExtend.test.js
@@ -4,28 +4,25 @@
 const BinaryTree = require('../treeExtend.js').BinaryTree;
 const Node = require('../treeExtend.js').Node;
 
-
-let testTree = new BinaryTree();
-testTree.root = new Node (2);
-testTree.root.left = new Node(7);
-testTree.root.right = new Node(5);
-testTree.root.left.left = new Node(2);
-testTree.root.left.right = new Node(6);
-testTree.root.right.left = new Node(5);
-
-
+let tree = new BinaryTree();
+tree.root = new Node('A');
+tree.root.left = new Node('B');
+tree.root.right = new Node('C');
+tree.root.left.left = new Node('D');
+tree.root.left.right = new Node('E');
+tree.root.right.left = new Node('F');
 
 describe('Tests the fuctionality of breadth first function', () => {
   it('returns a list of the values in the tree in the order they were encountered', () => {
-    let str = testTree.breadthFirst();
-    // console.log('RESULTS OF BF FUNCTION', str);
-    expect(str).toStrictEqual([2, 7, 5 ,2, 6, 5]);
+    let str = tree.breadthFirst();
+    console.log('RESULTS OF BF FUNCTION', str);
+    expect(str).toStrictEqual(['A', 'B', 'C', 'D', 'E', 'F']);
   })
 
   it('edge case, can traverse an empty tree', () => {
     let nullTree = new BinaryTree();
     let str = nullTree.breadthFirst();
-    // console.log('SHOULD BE NULL', str);
+    console.log('SHOULD BE NULL', str);
     expect(str).toEqual(null);
   })
 });
