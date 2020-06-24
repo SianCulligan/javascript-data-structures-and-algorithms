e66fa5e3913c2849f51297d831a5b536a883f924
Sian Culligan <SianCulligan@gmail.com>
2020-06-23 20:03:42 -0700

    Refactored code & added more tests

--- code_challenges/401/hashtable/_tests/hashtable.test.js
+++ code_challenges/401/hashtable/_tests/hashtable.test.js
@@ -1,3 +1,34 @@
 'use strict';
 
-const hashTable = require('../hashtable.js');
+const HashTable = require('../hashtable.js');
+
+describe ('Hashtable tests', () => {
+  const map = new HashTable(200);
+
+  it('Adds a key/value to your hash table', () => {
+    map.add('dog', 'the best creature');
+    expect(map.contains('dog')).toBe(true);
+    let ind = map.hash('dog');
+    expect(map.map[ind].val).toEqual('the best creature');
+  });
+
+  it('Can handle a collision', () => {
+    map.add('god', 'the way it feels when you get code working');
+    expect(map.contains('dog')).toBe(true);
+    expect(map.contains('god')).toBe(true);
+    let ind = map.hash('god');
+    let indTwo = map.hash('dog');
+    expect(ind).toBe(indTwo);
+    expect(map.map[ind].val).toEqual('the best creature');
+    expect(map.map[ind].next.val).toEqual('the way it feels when you get code working');
+  });
+
+  it('Returns null for a key that does not exist', () => {
+    expect(map.contains('cat')).toBe(false);
+  });
+
+});
+
+
+
+
--- code_challenges/401/hashtable/hashtable.js
+++ code_challenges/401/hashtable/hashtable.js
@@ -1,25 +1,75 @@
 'use strict';
 
-new HashTable {
-    constructor()
 
-    add () {
-    
+class HashTable {
+  constructor(initialSize) {
+    this.map = new Array(initialSize);
+  }
+
+  add (key, value) {
+    let addIndex = this.hash(key);
+
+    //build a check to avoid collisions
+    if (this.map[addIndex]) {
+      let current = this.map[addIndex];
+
+
+      while(current.next) {
+        if (current.key === key) {
+          if (current.val === value) {
+            return;
+          }
+          if (current.val.length) {
+            current.val.push(value);
+          } else {
+            current.val = [current.val, value];
+          }
+          return;
+        }
+        current = current.next;
+      }
+      current.next = {
+        key: key,
+        val: value,
+      };
     }
 
-    get () {
+    else
+      this.map[addIndex] = {
+        key: key,
+        val: value,
+      }
+  }
 
+  get (key) {
+    let ind = this.hash(key);
+
+    if(this.map[ind]) {
+      let current = this.map[ind]
+
+      while (current) {
+        if(current.key === key) return current.val;
+        current = current.next;
+      }
     }
+    return null;
+  }
 
-    contains () {
+  contains (key) {
+    let val = this.get(key);
+    if(!val) return false;
+    else return true;
+  }
 
+  hash (key) {
+    let sum = 0;
+    for (let i = 0; i < key.lenth; i++) {
+      sum += key.charCodeAt(i);
     }
-
-    hash () {
-
-    }
-
-
+    sum *= 599;
+    sum %= this.map.length;
+    return sum;
+  }
 }
 
-module.export = hashtable;
+module.exports = HashTable;
