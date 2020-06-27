7acc293229e66077167f590823559b5a7eee03a9
Sian Culligan <SianCulligan@gmail.com>
2020-06-27 12:34:37 -0700

    completed code with hashing and tests

--- code_challenges/401/repeatedWord/repeated-word.js
+++ code_challenges/401/repeatedWord/repeated-word.js
@@ -1,27 +1,39 @@
 // Write a function that accepts a lengthy string parameter.
 // Without utilizing any of the built-in library methods available to your language, return the first word to occur more than once in that provided string.
 
-function repeatedWord (string) {
-  let max = 0;
-  let mostRepeatedWord = '';
-  let splitWords = string.split(' ');
-  for(let i = 0; i < splitWords.length; i++) {
+'use strict';
 
-    let count = 0;
-    let word = '';
-    for(let j = 0; j < splitWords.length; j++) {
-      if(j !== i && splitWords[i] === splitWords[j]) {
-        count++;
-        word = splitWords[i];
+function hash(key, size) {
+  let sum = 0;
+  for (let i in key) sum += key.charCodeAt(i);
+  sum *= 599;
+  return sum % size;
+}
+
+
+function repeatedWord(str) {
+  let words = str.split(/[., -!?]/g);
+  let hashmap = new Array(words.length * 5);
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i] === '') continue;
+    let indx = hash(words[i].toLowerCase(), hashmap.length);
+    if (hashmap[indx]) {
+      let item = hashmap[indx];
+      while (item) {
+        if (item.key === words[i].toLowerCase()) return item.key;
+        item = item.next;
       }
-    }
 
-    if (count > mostRepeatedWord) {
-      max = count;
-      mostRepeatedWord = word;
+      hashmap[indx] = {
+        key: words[i].toLowerCase(),
+        next: hashmap[indx],
+      };
+    } else {
+      hashmap[indx] = { key: words[i].toLowerCase() };
     }
   }
-  return mostRepeatedWord;
+  return null;
 }
 
 module.exports = repeatedWord;
--- code_challenges/401/repeatedWord/repeated-word.test.js
+++ code_challenges/401/repeatedWord/repeated-word.test.js
@@ -3,3 +3,18 @@
 
 //OUTPUT
 //a
+
+
+'use strict';
+
+const repeatedWord = require('./repeated-word.js');
+
+describe('Testing repeated word', () => {
+  test('happy path ', () => {
+    let str = repeatedWord('Once upon a time, there was a brave princess who...');
+    expect(str).toBe('a');
+  });
+  test('expected failure ', () => {
+    expect(repeatedWord('769532')).toBeFalsey;
+  });
+});
