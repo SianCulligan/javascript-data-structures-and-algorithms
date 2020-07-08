aa2b6435bd2366f6381d06c7298706e3f42d189f
Sian Culligan <SianCulligan@gmail.com>
2020-07-07 17:57:32 -0700

    code added

---
+++ code_challenges/401/leftJoin/left-join.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function leftJoin (hashtableOne, hashtableTwo) {
+  if (!hashtableOne.size && !hashtableTwo.size) {
+    return [];
+  }
+  let results = [];
+  for (let [key, value] of hashtableOne) {
+    let join = [];
+    join.push(key, value);
+    hashtableTwo.has(key) ? join.push(hashtableTwo.get(key)) : join.push(null);
+    results.push(join);
+  }
+  return results;
+
+}
+
+module.exports = leftJoin;
