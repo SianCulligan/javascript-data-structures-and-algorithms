5de05ec7c0da9f1dc7809a053706a2636977a3a2
Sian Culligan <SianCulligan@gmail.com>
2020-07-07 18:09:23 -0700

    code works, updating tests

--- code_challenges/401/leftJoin/left-join.js
+++ code_challenges/401/leftJoin/left-join.js
@@ -7,12 +7,12 @@ function leftJoin (hashtableOne, hashtableTwo) {
   let results = [];
   for (let [key, value] of hashtableOne) {
     let join = [];
+    console.log('Status of Join: ', join);
     join.push(key, value);
     hashtableTwo.has(key) ? join.push(hashtableTwo.get(key)) : join.push(null);
     results.push(join);
   }
   return results;
-
 }
 
 module.exports = leftJoin;
